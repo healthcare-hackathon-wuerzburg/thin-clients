b34f998a758804830857a750d4d022c734dc75ca
BrandnerKasper <tobias.brandner@stud-mail.uni-wuerzburg.de>
2024-01-26 13:53:32 +0100

    edit labels and validate

--- code/validate.py
+++ code/validate.py
@@ -1,4 +1,5 @@
 import warnings
+
 warnings.filterwarnings('ignore')
 
 import torch
@@ -11,8 +12,10 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from dataset import CustomDataset
 from model import SimpleModel
 
+
 def round_with_threshold(tensor, threshold):
     return (tensor >= threshold).int()
+
 
 def main() -> None:
     # Loading and preparing data
@@ -33,7 +36,7 @@ def main() -> None:
     model.load_state_dict(torch.load(model_path))
     model.eval()
 
-    #initialize empty result arrays
+    # initialize empty result arrays
     all_targets = []
     all_outputs = []
 
@@ -45,7 +48,7 @@ def main() -> None:
         # round output to integer values
         output_rounded = round_with_threshold(output, 0.5)
 
-        #convert to numpy to be able to use sklearn performance metrics
+        # convert to numpy to be able to use sklearn performance metrics
         target_np = target.int().cpu().view(-1).numpy()
         output_rounded_np = output_rounded.cpu().view(-1).numpy()
 
@@ -53,7 +56,7 @@ def main() -> None:
         all_targets.append(target_np)
         all_outputs.append(output_rounded_np)
 
-    #define averaging method for precision recall and f1 scores
+    # define averaging method for precision recall and f1 scores
     avg_method = "weighted"
 
     # calculate sklearn performance metrics
@@ -67,6 +70,7 @@ def main() -> None:
     print(f"Precision: {precision}")
     print(f"Recall: {recall}")
     print(f"F1 Score: {f1}")
+
 
 if __name__ == '__main__':
     main()
